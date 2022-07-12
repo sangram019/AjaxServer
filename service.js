c0132190e5c7a339da2b753a34ca55b8f2793369
Sangram <sangram15.behera@gmail.com>
2022-07-13 02:52:17 +0530

    test Service using json

---
+++ service.js
@@ -0,0 +1,31 @@
+const { reject } = require("assert");
+
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise (function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        console.log(methodType+ "State changed called at: "+showTime()+" Ready state: " + xhr.readyState+ " Status: "+xhr.status);
+        if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+            resolve(xhr.responseText);
+        }   else if (xhr.status.toString().match('^[4,5][0,9]{2}$')) {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+            console.log("XHR Failed");
+        }
+    }
+    xhr.onerror = function () {
+        reject({
+            status: this.status,
+            statusText: xhttp.statusText
+        });
+    };
+    xhr.open(methodType, url, async); 
+    if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    } else xhr.send();
+    console.log(methodType+" request sent to the server at: "+showTime());
+    });
+}
